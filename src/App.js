90566abc3cc2c4c76a144a7932eb96b6edbb0245
shalurai <shaluraics22@gmail.com>
2017-10-30 19:22:55 +0900

    getting data from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import  * as BooksApi from './utils/BooksAPI'
 
 
 class App extends Component {
+  state ={
+    books:[]
+  }
+
+  componentDidMount() {
+    BooksApi.getAll().then((books) => {
+      this.setState({ books })
+      console.log(books);
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Books App</h1>
+
+        {this.state.books.map(books =>
+          <div key={books.id}>
+          <ol>
+        <p>{books.author}</p>
+        <p>{books.name} </p>
+        <p>{books.price}</p>
+          </ol>
+          </div>
+        )}
+
       </div>
     );
   }
---
+++ src/utils/BooksAPI.js
@@ -0,0 +1,26 @@
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
+
+let token = localStorage.token
+
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+export const getAll = () =>
+  fetch(`${api}/books`, { headers })
+    .then(res => res.json())
+    .then(data => data.books)
+
+export const create = (body) =>
+  fetch(`${api}/books`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(res => res.json())
